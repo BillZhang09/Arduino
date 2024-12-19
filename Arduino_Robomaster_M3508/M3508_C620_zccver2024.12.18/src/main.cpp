15b4598c423e04024b9ae9b344f09d9e760c8250
立风予沐 <159001802+ZhangChuChi@users.noreply.github.com>
2024-12-19 20:22:16 +0800

    Update main.cpp
    delete useless notes

--- Arduino_Robomaster_M3508/M3508_C620_zccver2024.12.18/src/main.cpp
+++ Arduino_Robomaster_M3508/M3508_C620_zccver2024.12.18/src/main.cpp
@@ -10,15 +10,10 @@ M3508_C620 M1;
 void setup() {
   Serial.begin(115200);
   M1.init();
-  
-    
-  /*canMsg1.data[0] = highByte(-1000);
-  canMsg1.data[1] = lowByte(1000);*/
  
 }
 
 void loop() {
   M1.setCurrentSpeed(M1.motorPID(-3000,3000,2000,M1.readMotor()));
-  //Serial.println(M1.motorPID(500,M1.readMotor()));
   Serial.println(M1.readMotor());
 }
